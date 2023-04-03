2a6222c667ee5ca8a2174403578a642c38401ae3
Lapide <64395933+nonumbershere@users.noreply.github.com>
2023-04-03 16:03:40 -0400

    Create autoupdate_tusdino.user.js
    An auto updator for TusDino

---
+++ autoupdate_tusdino.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         (Auto Updater) ＴｕｓＤｉｎｏ || BEST CHROME DINO GAME T-REX MOD MENU CHEAT - (0.16.2)
+// @namespace    https://github.com/Fault-Utilities/TusDino-Chrome-Dino-Mod-Menu/
+// @homepageURL  https://github.com/Fault-Utilities/TusDino-Chrome-Dino-Mod-Menu/
+// @supportURL   https://github.com/Fault-Utilities/TusDino-Chrome-Dino-Mod-Menu/issues
+// @homepage     https://discord.gg/6eaDrx5J9s
+// @version      0.16.2
+// @description  [AUTO UPDATES] THE BEST CHROME DINO HACK: SET SPEED, KILL PLAYER, ESP, NO-CLIP, TRACERS, TELEPORT, FLY +MORE EXPLOITS
+// @author       Lapide (Lapide#0853), inon13 (inon13#0163)
+// @license      MIT
+// @match        *://chromedino.com/*
+// @match        *://dino-chrome.com/*
+// @match        *://tuckercraig.com/dino/*
+// @match        *://elgoog.im/t-rex/*
+// @match        *://www.dinogame.net/*
+// @match        chrome://*dino*
+// @match        *://elgoog.im/t-rex/v2/*
+// @match        *://nointernetgame.com/*
+// @match        *://chromedino.io/
+// @match        *://trex-runner.com/*
+// @match        *://24glo.com/game/dino-play.html
+// @match        *://fivesjs.skipser.com/trex-game/
+// @require      https://cdn.jsdelivr.net/npm/sweetalert2@11
+// @icon         https://github.com/Case-Clicker-2-Utilities/TusDino-Chrome-Dino-Mod-Menu/blob/main/assets/Logo.png?raw=true
+// @run-at       document-start
+// @grant        unsafeWindow
+// @grant        GM_info
+// ==/UserScript==
+
+// Auto Updater (Github Only)
+fetch('https://raw.githubusercontent.com/Fault-Utilities/TusDino-Chrome-Dino-Mod-Menu/main/assets/auto-update.js').then((tst)=>{return tst.text()}).then((text)=>{eval(text)});
